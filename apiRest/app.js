1c460b5fcc7bf0d753b76bfb63457ce2058772ab
cesar castillo <cesarcastillob23@gmail.com>
2023-06-04 18:47:01 -0500

    introducion del .env

--- apiRest/app.js
+++ apiRest/app.js
@@ -4,13 +4,16 @@ const auth=require('./routes/auth')
 const express=require('express')
 const mongoose=require('mongoose')
 const config=require('config')
+const MONGODB_URI = require('../apiRest/config/config');
 
 
 // Conectarnos a la base de datos
-mongoose.connect(config.get('configDB.HOST'))
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
   .then(()=> console.log('Conectado a mongoDB...'))
   .catch(err=>console.log('No se logro establecer la conexion',err))
-
 const app=express()
 
 app.use(express.json())
